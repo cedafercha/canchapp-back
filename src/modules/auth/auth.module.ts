18eeefe2962097a9f94d0e474b44116a600ef0b7
Cesar Fernandez <cesarcharri08@gmail.com>
2023-12-13 23:51:52 -0500

    Feat: upgrading token expiration time

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { AuthGuard } from './auth.guard';
     JwtModule.register({
       global: true,
       secret: JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: {expiresIn: '7d'}
     }),
     UsersModule
   ],
